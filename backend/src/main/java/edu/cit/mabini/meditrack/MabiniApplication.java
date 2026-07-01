f79187592726e85b22c06b43f03cb6df13c64f03
LAB22_USER ON WS12 <L22X16W12@LabHCI.net>
2026-07-01 16:49:50 +0800

    Initial project setup for MediTrack

---
+++ backend/src/main/java/edu/cit/mabini/meditrack/MabiniApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.mabini.meditrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MabiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MabiniApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/edu/cit/mabini/meditrack/MabiniApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.mabini.meditrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MabiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
